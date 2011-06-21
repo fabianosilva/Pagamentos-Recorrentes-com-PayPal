f9f6edd5e1df6e97d58e7b2735e8e390f2e5495d
Fabiano Silva <fabiano.silva@gmail.com>
2011-06-20 23:11:28 -0300

    Versao Inicial

--- WebContent/checkout.php
+++ WebContent/checkout.php
@@ -5,16 +5,16 @@ $nvp = array(
 	'PAYMENTREQUEST_0_AMT'			=> $total,
 	'PAYMENTREQUEST_0_CURRENCYCODE'		=> 'BRL',
 	'PAYMENTREQUEST_0_PAYMENTACTION'	=> 'Sale',
-        'LOCALECODE'                            => 'pt_BR', //pt_BR forca experiencia em portugues
-        'L_BILLINGTYPE0'                        => 'RecurringPayments', //Nececssario informar o tipo de pagamento
-        'L_BILLINGAGREEMENTDESCRIPTION0'        => 'Acesso a conteúdo exclusivo. R$9,90 por mês.', //adicione uma descricao da assinatura
+    'LOCALECODE'                            => 'pt_BR', //pt_BR forca experiencia em portugues
+    'L_BILLINGTYPE0'                        => 'RecurringPayments', //Nececssario informar o tipo de pagamento
+    'L_BILLINGAGREEMENTDESCRIPTION0'        => 'Acesso a conteúdo exclusivo. R$9,90 por mês.', //adicione uma descricao da assinatura
 	'RETURNURL'				=> 'http://127.0.0.1/RecurringPayments/retorno.php?PERIODO='.$_GET['periodo'],
 	'CANCELURL'				=> 'http://127.0.0.1/RecurringPayments/cancelamento.php',
 	'METHOD'				=> 'SetExpressCheckout',
 	'VERSION'				=> '72.0',
-	'PWD'					=> 'XBCECC7M9WPS4S35',
-	'USER'					=> 'dono_api1.apenasteste.com',
-	'SIGNATURE'				=> 'AFcWxV21C7fd0v3bYYYRCpSSRl31AGcMV-gr7nQOpmCIExDJowMyu.4c',
+	'PWD'					=> 'PWD',
+	'USER'					=> 'USR',
+	'SIGNATURE'				=> 'SIGNATURE',
 
 
 );
